44b816de6a85098ea0c4bf1190e8133e84d0bd98
dogdie233 <dogdie233@163.com>
2024-12-02 03:03:39 +0800

    [Example] chore: Update usage example [Example] add: a program to generate the magic number in SimpleCasCaptchaResolver`

--- OhMyDearGpnu/Program.cs
+++ OhMyDearGpnu/Program.cs
@@ -2,6 +2,7 @@
 using OhMyDearGpnu.Api;
 using OhMyDearGpnu.Api.AcaAff;
 using OhMyDearGpnu.Api.AcaAff.Requests;
+using OhMyDearGpnu.Api.Cas;
 using OhMyDearGpnu.Api.IoT;
 
 var client = new GpnuClient();
@@ -30,13 +31,18 @@ else
     }
 
     Console.WriteLine("已将验证码保存至casCaptcha.png");
-    Console.Write("请输入验证码结果: ");
+    Console.Write("请输入验证码结果（留空则自动填充）: ");
     casCaptcha.value = Console.ReadLine();
-    var casLoginRes = await casHandler.LoginByPassword(username, password, casCaptcha, true);
-    if (!casLoginRes.IsSucceeded)
+    if (string.IsNullOrEmpty(casCaptcha.value))
+        casCaptcha.value = null;
+    try
     {
-        Console.WriteLine($"cas 登录失败，原因：{casLoginRes.ErrorMessage}");
-        return;
+        var casLoginRes = await casHandler.LoginByPassword(username, password, casCaptcha, true);
+    }
+    catch (CasLoginFailException e)
+    {
+        Console.WriteLine($"cas 登录失败，原因：{e.Message}");
+        throw;
     }
 }
 
@@ -44,64 +50,28 @@ Console.WriteLine($"cas 登录成功，TGT为{casHandler.Tgt}");
 
 var iot = client.GetIoTContext();
 var myRooms = await iot.ListMyRoomElectric();
-var roomCode = myRooms.data![0].RoomCode;
+var roomCode = myRooms[0].RoomCode;
 var electricBalance = await iot.GetElectricBalance(roomCode);
-Console.WriteLine($"你的房间是：{roomCode}，电费余额为{electricBalance.data!.MoneyBalance}");
+Console.WriteLine($"你的房间是：{roomCode}，电费余额为{electricBalance.MoneyBalance}");
 
-if (username == null || password == null)
+var acaAff = client.GetAcaAffContext();
+Console.WriteLine("正在获取个人信息");
+var personInfoResponse = await client.SendRequest(new PersonInfoRequest());
+Console.WriteLine($"你好，{personInfoResponse.Name}({personInfoResponse.StudentID})");
+
+Console.WriteLine("正在获取日历");
+var calendar = await acaAff.GetCalendar();
+Console.WriteLine($"本周为第{calendar.CurrentWeek}周");
+var curriculums = await acaAff.GetCurriculums(2023, "3");
+Console.WriteLine($"========本周的课表为========");
+foreach (var curriculum in curriculums)
 {
-    Console.Write("请输入学号: ");
-    username = Console.ReadLine()!;
-    Console.Write("请输入密码: ");
-    password = Helper.ReadPassword();
+    if (!curriculum.Week.IsInRange(calendar.CurrentWeek))
+        continue;
+    Console.Write($"周{curriculum.Day}    ");
+    Console.Write($"{curriculum.Name.PadRight(20, ' ')}");
+    Console.Write($"{curriculum.Classroom.PadRight(10, ' ')}");
+    Console.WriteLine();
 }
 
-Console.WriteLine("正在获取验证码");
-var captcha = (await client.AcaAffGetCaptcha()).data!;
-File.Delete("captcha.jpg");
-using (var fs = File.Create("captcha.jpg"))
-{
-    await captcha.imageStream.CopyToAsync(fs);
-}
-
-Console.WriteLine("已将验证码保存至captcha.jpg");
-Console.Write("请输入验证码: ");
-captcha.value = Console.ReadLine();
-var loginRes = await client.AcaAffLogin(username, password, captcha);
-if (loginRes.IsSucceeded)
-{
-    Console.WriteLine("登录成功，正在获取个人信息");
-    var personInfoResponse = await client.SendRequest(new PersonInfoRequest());
-    if (personInfoResponse.IsSucceeded)
-    {
-        Console.WriteLine($"你好，{personInfoResponse.data!.Name}({personInfoResponse.data.StudentID})");
-    }
-    else
-    {
-        Console.WriteLine($"获取个人信息失败");
-        Environment.Exit(0);
-    }
-
-    Console.WriteLine("正在获取日历");
-    var calendarRes = await client.AcaAffGetCalendar();
-    var calendar = calendarRes.data!;
-    Console.WriteLine($"本周为第{calendar.CurrentWeek}周");
-    var curriculumsRes = await client.AcaAffGetCurriculums(2023, "3");
-    var curriculums = curriculumsRes.data!;
-    Console.WriteLine($"========本周的课表为========");
-    foreach (var curriculum in curriculums)
-    {
-        if (!curriculum.Week.IsInRange(calendar.CurrentWeek))
-            continue;
-        Console.Write($"周{curriculum.Day}    ");
-        Console.Write($"{curriculum.Name.PadRight(20, ' ')}");
-        Console.Write($"{curriculum.Classroom.PadRight(10, ' ')}");
-        Console.WriteLine();
-    }
-
-    Console.WriteLine("============================");
-}
-else
-{
-    Console.WriteLine($"登录失败，{loginRes.message}");
-}
+Console.WriteLine("============================");
---
+++ OhMyDearGpnu/SimpleCasCaptchaResolverBestWayFindProgram.cs
@@ -0,0 +1,73 @@
+﻿using OhMyDearGpnu.Api.Common.Drawing;
+
+namespace OhMyDearGpnu;
+
+internal class SimpleCasCaptchaResolverBestWayFindProgram
+{
+    internal static void Entry(string[] args)
+    {
+        var folder = "captcha/";
+        var arrays = new List<Group>();
+        for (var i = 0; i < 10; i++)
+        {
+            using var fs = File.OpenRead(Path.Combine(folder, $"{i}.png"));
+            var image = Image.FromSimplePng(fs);
+            var white = image[0, 0];
+            arrays.Add(new Group($"第{i}", image.Pixels.Select(rgba => rgba == white ? 0 : 1).ToArray()));
+        }
+
+        var steps = ArraySplitter.MinStepsToSplit(arrays);
+        Console.WriteLine("最少步骤数: " + steps.Count);
+        Console.WriteLine("步骤: " + string.Join(", ", steps));
+    }
+
+    public record struct Group(string Name, int[] Value);
+
+    private static class ArraySplitter
+    {
+        public static List<int> MinStepsToSplit(List<Group> arrays)
+        {
+            var steps = new List<int>();
+            Split(arrays, 0, steps);
+            return steps;
+        }
+
+        private static void Split(List<Group> arrays, int step, List<int> steps)
+        {
+            if (arrays.Count <= 1) return;
+
+            var minSteps = int.MaxValue;
+            var bestIndex = -1;
+            List<Group>? bestGroup0 = null;
+            List<Group>? bestGroup1 = null;
+
+            for (var i = 0; i < arrays[0].Value.Length; i++)
+            {
+                var group0 = new List<Group>();
+                var group1 = new List<Group>();
+
+                foreach (var array in arrays)
+                    if (array.Value[i] == 0)
+                        group0.Add(array);
+                    else
+                        group1.Add(array);
+
+                var steps0 = group0.Count > 0 ? group0.Count : 0;
+                var steps1 = group1.Count > 0 ? group1.Count : 0;
+
+                var maxSteps = Math.Max(steps0, steps1);
+                if (maxSteps >= minSteps)
+                    continue;
+                minSteps = maxSteps;
+                bestIndex = i;
+                bestGroup0 = group0;
+                bestGroup1 = group1;
+            }
+
+            steps.Add(bestIndex);
+            Console.WriteLine($"经过第{step}步分割，第{bestIndex}个元素分割，分组A为：{string.Join(", ", bestGroup0?.Select(g => g.Name) ?? Array.Empty<string>())}，分组B为：{string.Join(", ", bestGroup1?.Select(g => g.Name) ?? Array.Empty<string>())}");
+            Split(bestGroup0!, step + 1, steps);
+            Split(bestGroup1!, step + 1, steps);
+        }
+    }
+}
