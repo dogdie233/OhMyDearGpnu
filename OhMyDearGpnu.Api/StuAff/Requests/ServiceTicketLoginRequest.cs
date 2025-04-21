4c0818ab587c19fa79f055b0552ba78568e8425a
dogdie233 <dogdie233@163.com>
2025-04-21 17:54:30 +0800

    fix: StuAff module login failed

--- OhMyDearGpnu.Api/StuAff/Requests/ServiceTicketLoginRequest.cs
+++ OhMyDearGpnu.Api/StuAff/Requests/ServiceTicketLoginRequest.cs
@@ -8,12 +8,16 @@ public partial class ServiceTicketLoginRequest(string ticket) : BaseRequest
     public override Uri Url { get; } = new(Hosts.stuAff, $"xgsy/shiro-cas?ticket={ticket}");
     public override HttpMethod HttpMethod => HttpMethod.Get;
 
-    public override ValueTask EnsureResponse(SimpleServiceContainer serviceContainer, HttpResponseMessage responseMessage)
+    public override ValueTask EnsureResponse(SimpleServiceContainer serviceContainer,
+        HttpResponseMessage responseMessage)
     {
         if (responseMessage.RequestMessage!.RequestUri!.Host == Hosts.cas.Host)
             throw new UnexpectedResponseException("Unable to login to Student affairs, maybe service ticket invalid");
 
-        responseMessage.EnsureSuccessStatusCode();
+        if (responseMessage.RequestMessage.RequestUri is null) // fallback to check response status code
+            responseMessage.EnsureSuccessStatusCode();
+        else if (responseMessage.RequestMessage.RequestUri.Host == Hosts.cas.Host)
+            throw new UnexpectedResponseException("Unable to login to Student affairs, maybe service ticket invalid");
         return ValueTask.CompletedTask;
     }
 }
